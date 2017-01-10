bb9f0206dbd9f14d2b26ab92350c4f28f074998c
Rick Clark <rkingsleyclark@gmail.com>
2017-01-10 11:30:06 +0000

    unit test for deduct

---
+++ .pryrc
@@ -0,0 +1,3 @@
+require "./lib/oystercard.rb"
+
+oystercard = Oystercard.new
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,13 +3,13 @@ require 'oystercard'
 describe Oystercard do
   subject(:oystercard) { described_class.new }
 
-  describe ".balance" do
+  describe "#balance" do
     it 'initialises with a balance of 0' do
       expect(oystercard.balance).to eq 0
     end
   end
 
-  describe ".top_up" do
+  describe "#top_up" do
     it 'tops up by a given amount' do
       expect(oystercard.top_up(10)).to eq oystercard.balance
     end
@@ -18,6 +18,10 @@ describe Oystercard do
       maximum_bal = described_class::MAX_BALANCE
       expect{ oystercard.top_up(91) }.to raise_error("Balance cannot exceed #{maximum_bal}")
     end
+  end
+
+  describe "#deduct" do
+    it { is_expected.to respond_to(:deduct) }
   end
 
 end
