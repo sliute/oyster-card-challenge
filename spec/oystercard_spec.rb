2fa3cc3299d01bb2a7bced5f0a134eff7f25d5a2
Rick Clark <rkingsleyclark@gmail.com>
2017-01-10 09:48:48 +0000

    refactored rspec tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,22 +1,25 @@
 require 'oystercard'
 
 describe Oystercard do
-  it 'initialises with a balance of 0' do
-    #card = Oystercard.new
-    expect(subject.balance).to eq 0
+  subject(:oystercard) { described_class.new }
+
+  describe ".balance" do
+    it 'initialises with a balance of 0' do
+      #card = Oystercard.new
+      expect(oystercard.balance).to eq 0
+    end
   end
 
-  it 'tops up by a given amount' do
-    card = Oystercard.new
-    # card.top_up(10)
-    expect(card.top_up(10)).to eq card.balance
+  describe ".top_up" do
+    it 'tops up by a given amount' do
+      expect(oystercard.top_up(10)).to eq oystercard.balance
+    end
+
+    it 'throws error if balance exceeds 90' do
+      maximum_bal = described_class::MAX_BALANCE
+      expect{ oystercard.top_up(91) }.to raise_error("Balance cannot exceed #{maximum_bal}")
+    end
   end
 
-  it 'throws error if balance exceeds 90' do
-    card = Oystercard.new
-    maximum_bal = Oystercard::MAX_BALANCE
-    expect{ card.top_up(91) }.to raise_error("Balance cannot exceed #{maximum_bal}")
-
-  end
 
 end
